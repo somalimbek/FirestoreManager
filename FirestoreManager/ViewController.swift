a07ee3ca21d3436aeb9c84bb4a4570286e96448d
Soma Limbek <soma.limbek@gmail.com>
2020-05-15 12:24:21 +0200

    Initial Commit

---
+++ FirestoreManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreManager
+//
+//  Created by Limbek Soma on 2020. 05. 15..
+//  Copyright © 2020. Soma Limbek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
